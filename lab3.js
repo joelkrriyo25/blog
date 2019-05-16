1b751ecb7743d66d5ea66f5eb6d333b5da55b9fc
joelkrriyo25 <35752832+joelkrriyo25@users.noreply.github.com>
2019-05-16 11:20:19 -0500

    Update lab3.js

--- lab3.js
+++ lab3.js
@@ -1,8 +1,8 @@
 (function (){
     var Module = {
         Data:{
-            arr: undefined//,
-            //: undefined
+            arr: undefined,
+            almCant: undefined
         },
         Methods:{
             initExceptions: function()
@@ -22,7 +22,8 @@
                     }
 
                     
-                    if (isNaN(Module.Data.arr))
+                    //console.log(Module.Validacion.validaArregloNum());
+                    if (Module.Validacion.validaArregloNum())
                     {
  
                         throw new Module.Exceptions.UserExcepction("El Arreglo debe de tener solo número");
@@ -31,15 +32,69 @@
 
                 },
                 cal: function(){
-                    
-                    
-                    Module.Data.arr.forEach(function(element) {
-                        console.log(element);
-                      });
-                      
+                    var contados = [];
+                    var cantTotal = 0;
+
+                    for(let i = 0; i < Module.Data.arr.length; i +=1)
+                    {       
+                           
+                            if (contados.indexOf(Module.Data.arr[i]) < 0)
+                            {
+                                    var cantAux = 0
+                                    for(let y = 0; y < Module.Data.arr.length; y +=1)
+                                    {
+                                        if ( (Module.Data.arr[i] === Module.Data.arr[y]))
+                                        {
+                                        // console.log(Module.Data.arr[i]);
+                                            cantAux +=1;
+                                        }
+                                    }
+                                    
+                                    //console.log(Module.Data.arr[i], (cantAux%2) , cantAux);
+
+                                    contados.push(Module.Data.arr[i]);
+
+                                    if ((cantAux%2) == 0)
+                                    {
+                                        cantTotal +=cantAux/2;
+                                    }
+                                    else 
+                                    {
+                                        cantTotal +=Math.floor(cantAux/2);
+                                    }
+
+                                                            
+                        }
+
+                    }
+
+                    console.log('La cantidad de pares en el arreglo es de ',cantTotal);
 
                 }
-                
+
+        },
+        Validacion:{
+                validaArregloNum: function(){
+                    
+                    var find = false;
+                    for(let i = 0; i < Module.Data.arr.length; i +=1)
+                    {
+                        if (!Number.isInteger(Module.Data.arr[i]) )
+                        {
+                            find = true;
+                        }
+                        
+                    }
+
+                    if (find)
+                    {
+                        return true;
+                    }else 
+                    {
+                        return false;
+                    }
+                    
+                }
         },
         Exceptions:{
             UserExcepction: function(message){
@@ -49,7 +104,7 @@
             }
          }
     }
-    const nrospar = [5,4,1,9,25,5];
-    Module.Methods.init(nrospar);
+    const enteros =[1,1,1,1,1,2,2,2] ;//[10,5,4,4,4,1,1,10,10,10];
+    Module.Methods.init(enteros);
     Module.Methods.cal();
 })();
